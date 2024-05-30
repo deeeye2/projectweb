762ed244f0b49e83b3da81a0d180ebec6cd45a07
MOHAMED SESAY <162488876+deeeye2@users.noreply.github.com>
2024-05-30 16:07:26 +0200

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,4 +1,4 @@
-package com.deeeye.devopsweb;
+package com.deeeye.projectweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
